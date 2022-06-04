83b3c94b6220fea2bd96e91cb875b4a983a096cb
Danny Kaump <dkaump92@gmail.com>
2022-06-04 02:43:59 -0700

    dims fix

--- server.js
+++ server.js
@@ -6,45 +6,45 @@ const PORT = 8000
 app.use(cors())
 
 class Surfboard {
-    constructor(model, brand, condition, price, length, type) {
+    constructor(model, brand, condition, price, length, dims, type) {
         this.model = model || 'n/a'
         this.brand = brand || 'n/a'
         this.type = type || 'n/a'
         this.condition = condition || 'n/a'
         this.price = price || 0
         this.length = length || 'n/a'
+        if (dims) {
+            this.volume = dims[0]
+        }
     }
     getPrice = _ => `$${this.price}`
+    getVolume = _ => `${this.volume}L`
 }
 
 class Hybrid extends Surfboard {
-    constructor(model, brand, condition, price, length) {
-        super(model, brand, condition, price, length)
+    constructor(model, brand, condition, price, length, dims) {
+        super(model, brand, condition, price, length, dims)
         this.type = 'hybrid'
     }
 }
 
 class Shortboard extends Surfboard {
-    constructor(model, brand, condition, price, length) {
-        super(model, brand, condition, price, length)
+    constructor(model, brand, condition, price, length, dims) {
+        super(model, brand, condition, price, length, dims)
         this.type = 'shortboard'
     }
 }
 
 class Longboard extends Surfboard {
-    constructor(model, brand, condition, price, length) {
-        super(model, brand, condition, price, length)
+    constructor(model, brand, condition, price, length, dims) {
+        super(model, brand, condition, price, length, dims)
         this.type = 'longboard'
     }
 }
 
-const whitenoiz = new Shortboard('White Noiz', 'HS', 'used', 200, {
-    '5\'10': {
-        'volume': 28.5
-    }
-})
+const whitenoiz = new Shortboard('White Noiz', 'HS', 'used', 200, '5\'10', [28.5])
 
-const dreamcatcher = new Hybrid('Dreamcatcher', 'Robert\'s', 'good', 1000, new Dims('6\'3', [38]))
+const dreamcatcher = new Hybrid('Dreamcatcher', 'Robert\'s', 'good', 1000, '6\'3', [38])
 
 const unknown = new Surfboard()
 
@@ -53,16 +53,6 @@ const surfboards = [
     dreamcatcher,
     unknown
 ]
-
-class Dims {
-    constructor(length, dims) {
-        this.length = length
-        this.length.dims = {
-            'volume': dims[0] || 'na'
-        }
-    }
-}
-
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
@@ -89,4 +79,5 @@ app.listen(process.env.PORT || PORT, () => {
 
 setTimeout(() => {
     console.log(dreamcatcher.getPrice())
+    console.log(dreamcatcher.getVolume())
 }, 1000)
