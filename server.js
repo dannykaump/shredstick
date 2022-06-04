69e5cacf00b279feacec4192e0c2a77f6e2362d6
Danny Kaump <dkaump92@gmail.com>
2022-06-04 00:57:40 -0700

    used boards

--- server.js
+++ server.js
@@ -6,60 +6,82 @@ const PORT = 8000
 app.use(cors())
 
 class Surfboard {
-    constructor(model, brand, type, length) {
-        this.model = model || 'unknown'
-        this.brand = brand || 'unknown'
-        this.type = type || 'unknown'
-        this.length = length || 'unknown'
+    constructor(model, brand, condition, price, length, type) {
+        this.model = model || 'n/a'
+        this.brand = brand || 'n/a'
+        this.type = type || 'n/a'
+        this.length = length || 'n/a'
+        this.condition = condition || 'n/a'
+        this._price = price || 0
+        this.price = this._price
     }
+    getPrice = _ => `$${this.price}`
 }
 
 class Hybrid extends Surfboard {
-    constructor(model, brand, length) {
-        super(model, brand, length)
+    constructor(model, brand, condition, price, length) {
+        super(model, brand, condition, price, length)
         this.type = 'hybrid'
     }
 }
 
-const whitenoiz = new Surfboard('White Noiz', 'HS', 'hpsb', {
-    '5\'10' : {
-        'volume' : 28.5
+class Shortboard extends Surfboard {
+    constructor(model, brand, condition, price, length) {
+        super(model, brand, condition, price, length)
+        this.type = 'shortboard'
+    }
+}
+
+class Longboard extends Surfboard {
+    constructor(model, brand, condition, price, length) {
+        super(model, brand, condition, price, length)
+        this.type = 'longboard'
+    }
+}
+
+const whitenoiz = new Shortboard('White Noiz', 'HS', 'used', 200, {
+    '5\'10': {
+        'volume': 28.5
     }
 })
 
-const dreamcatcher = new Hybrid('Dreamcatcher', 'Robert\'s', {
+const dreamcatcher = new Hybrid('Dreamcatcher', 'Robert\'s', 'good', 1000, {
     '6\'3': {
-        'volume' : 38
+        'volume': 38
     }
 })
 
 const unknown = new Surfboard()
 
-const surfboards = {
+const surfboards = [
     whitenoiz,
     dreamcatcher,
     unknown
-}
+]
 
-app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/index.html')
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api', (request, response) => {
-    response.json(surfboards)
+app.get('/api', (req, res) => {
+    res.json(surfboards)
 })
 
-app.get('/api/:name', (request, response) => {
-    const boardName = request.params.name.split(' ').join('').toLowerCase()
+app.get('/api/:name', (req, res) => {
+    const boardName = req.params.name.split(' ').join('').toLowerCase()
 
     if (surfboards[boardName]) {
-        response.json(surfboards[boardName])
+        res.json(surfboards[boardName])
     } else {
-        response.json(unknown)
+        res.json(unknown)
     }
 
 })
 
 app.listen(process.env.PORT || PORT, () => {
-    console.log(`The server is now running on port ${PORT}! Betta Go Catch It!`)
+    console.log(`The server is now firing on port ${PORT}!`)
 })
+
+setTimeout(() => {
+    console.log(dreamcatcher.getPrice())
+}, 1000)
