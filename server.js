ed726216e064c8fc22f00ae4124d3812a65a8ec8
Danny Kaump <dkaump92@gmail.com>
2022-06-07 17:17:19 -0700

    public

--- server.js
+++ server.js
@@ -89,39 +89,3 @@ MongoClient.connect('mongodb+srv://danielrkaump:hamden1216@cluster0.ejksv.mongod
 
     .catch(error => console.error(error))
 
-
-// class Surfboard {
-//     constructor(info, dims, fins) {
-//         if (info) {
-//             this.model = info[0]
-//             this.brand = info[1]
-//             this.type = info[2]
-//             this.condition = info[3]
-//             this.price = info[4]
-//         }
-//         if (dims) {
-//             this.length = dims[0]
-//             this.volume = dims[1]
-//         }
-//         if (fins) {
-//             this.finType = fins[0]
-//             this.finBox = fins[1]
-//         }
-//     }
-//     getPrice = _ => `$${this.price}`
-//     getVolume = _ => `${this.volume}L`
-// }
-
-//placeholder literal notaysh
-// const whitenoiz = new Surfboard(['White Noiz', 'HS', 'shortboard', 'used', 200], ['5\'10', 28.5], ['futures', 'thruster'])
-
-// const dreamcatcher = new Surfboard(['Dreamcatcher', 'Robert\'s', 'hybrid', 'good', 1000], ['6\'3', 38], ['futures', '5-fin'])
-
-// const unknown = new Surfboard()
-
-// const surfboards = [
-//     whitenoiz,
-//     dreamcatcher,
-//     unknown
-// ]
-
